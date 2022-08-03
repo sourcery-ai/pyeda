f77e90dcd4d2f1bf015b9dcb764a4c7cda7a98eb
Ruan Comelli <ruancomelli@gmail.com>
2022-08-02 23:33:59 -0300

    test(pyeda/logic/test/test_sudoku): fix CWD calculation

--- pyeda/logic/test/test_sudoku.py
+++ pyeda/logic/test/test_sudoku.py
@@ -3,18 +3,18 @@ Test logic functions for Sudoku
 """
 
 import os
-import random
 
 from pyeda.logic.sudoku import SudokuSolver
 
-
 SOLVER = SudokuSolver()
 
-cwd, _ = os.path.split(__file__)
+cwd = os.path.dirname(__file__)
 top95_txt = os.path.join(cwd, 'top95.txt')
 top95_solns_txt = os.path.join(cwd, 'top95_solns.txt')
+
 with open(top95_txt) as fin:
     GRIDS = [line.strip() for line in fin]
+
 with open(top95_solns_txt) as fin:
     SOLNS = [line.strip() for line in fin]
 
